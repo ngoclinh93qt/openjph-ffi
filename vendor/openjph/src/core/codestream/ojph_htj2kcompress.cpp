ff2ac57dcb0c29fd7ded9e106d3a35caa4e4efd4
Linh <linhnguyen@zentech.io>
2021-08-25 16:08:14 +0700

    debug

--- vendor/openjph/src/core/codestream/ojph_htj2kcompress.cpp
+++ vendor/openjph/src/core/codestream/ojph_htj2kcompress.cpp
@@ -168,65 +168,9 @@ namespace ojph {
     return out;
   }
 
-  const ui8* htj2kcompress::encodedao(const uint8_t* data, size_t width, size_t height, bool isSigned) {
+  int htj2kcompress::encodedao(const uint8_t* data, size_t width, size_t height, bool isSigned) {
 
-    ojph::codestream codestream;
-    ojph::param_siz siz = codestream.access_siz();
-    siz.set_image_extent(ojph::point(width, height));
-    int num_comps = 3;
-    siz.set_num_components(num_comps);
-    for (int c = 0; c < num_comps; ++c)
-      siz.set_component(c, ojph::point(1, 1), 8, false);
-    siz.set_image_offset(ojph::point(0, 0));
-    siz.set_tile_size(ojph::size(0, 0));
-    siz.set_tile_offset(ojph::point(0, 0));
-    ojph::param_cod cod = codestream.access_cod();
-
-    cod.set_num_decomposition(5);
-    cod.set_block_dims(64, 64);
-
-
-    const char* progOrders[] = { "LRCP", "RLCP", "RPCL", "PCRL", "CPRL" };
-    cod.set_progression_order(progOrders[4]);
-    cod.set_color_transform(true);
-    cod.set_reversible(false);
-    codestream.access_qcd().set_irrev_quant(0.5);
-    mem_outfile output;
-    output.open();
-    codestream.set_planar(false);
-    codestream.write_headers(&output);
-
-
-    const size_t bytesPerPixel = 3;
-    int next_comp;
-    ojph::line_buf* cur_line = codestream.exchange(NULL, next_comp);
-
-
-    for (size_t y = 0; y < height; y++)
-    {
-      for (size_t c = 0; c < siz.get_num_components(); c++)
-      {
-
-        assert(c == next_comp);
-
-
-        read(cur_line, data, width, y, next_comp);
-
-        cur_line = codestream.exchange(cur_line, next_comp);
-
-
-      }
-
-    }
-
-
-
-    // cleanup
-    codestream.flush();
-    codestream.close();
-
-
-    return output.get_data();
+   return 1;
   }
 
 

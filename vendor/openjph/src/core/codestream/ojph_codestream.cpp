cad1294a84b60609996a3974f2f2d1fce5d091f9
Linh <linhnguyen@zentech.io>
2021-08-27 11:22:44 +0700

    test

--- vendor/openjph/src/core/codestream/ojph_codestream.cpp
+++ vendor/openjph/src/core/codestream/ojph_codestream.cpp
@@ -1171,8 +1171,9 @@ namespace ojph {
           for (int i = 0; i < num_tiles.w; ++i)
           {
             int idx = i + cur_tile_row * num_tiles.w;
-            if ((tiles[idx].push(line, cur_comp)))
-              break;
+            success &= tiles[idx].push(line, cur_comp)
+            // if ((success &= tiles[idx].push(line, cur_comp)) == false)
+            //   break;
           }
           cur_tile_row += success == false ? 1 : 0;
           if (cur_tile_row >= num_tiles.h)

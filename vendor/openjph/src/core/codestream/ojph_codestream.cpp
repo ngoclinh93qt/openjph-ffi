2aaf2a8591904068291e93c02f3031281e99efe0
Linh <linhnguyen@zentech.io>
2021-08-27 14:27:31 +0700

    test

--- vendor/openjph/src/core/codestream/ojph_codestream.cpp
+++ vendor/openjph/src/core/codestream/ojph_codestream.cpp
@@ -1492,9 +1492,9 @@ namespace ojph {
              comps[0].get_line()->i32,
              comps[1].get_line()->i32,
              comps[2].get_line()->i32, comp_width);
-             comps[0].push_line();
-             comps[1].push_line();
-             comps[2].push_line();
+            //  comps[0].push_line();
+            //  comps[1].push_line();
+            //  comps[2].push_line();
           }
        // }
         // else

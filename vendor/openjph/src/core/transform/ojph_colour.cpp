7f59a210c4ae74d36ddf1210fc7d7efc4127dcd7
Dao Cuong <daocuong@gmail.com>
2021-08-30 12:25:03 -0500

    fix

--- vendor/openjph/src/core/transform/ojph_colour.cpp
+++ vendor/openjph/src/core/transform/ojph_colour.cpp
@@ -182,12 +182,14 @@ namespace ojph {
     void gen_cnvrt_si32_to_float_shftd(const si32 *sp, float *dp, float mul,
                                        int width)
     {
+      printf("hi");
       for (int i = width; i > 0; --i) {
-        printf(" %d \n",i);
         *dp++ = (float)*sp++ * mul - 0.5f;
       }
+       printf("dao");
 
-      printf("here\n");
+      
+
     }
 
     //////////////////////////////////////////////////////////////////////////

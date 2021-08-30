7ddadb4a5dd15716f66d6fb6feff1f16f7f4937c
Dao Cuong <daocuong@gmail.com>
2021-08-30 10:48:20 -0500

    fix error

--- vendor/openjph/src/core/transform/ojph_colour.cpp
+++ vendor/openjph/src/core/transform/ojph_colour.cpp
@@ -182,9 +182,10 @@ namespace ojph {
     void gen_cnvrt_si32_to_float_shftd(const si32 *sp, float *dp, float mul,
                                        int width)
     {
-      for (int i = width; i > 0; --i)
+      for (int i = width; i > 0; --i) {
+        printf(" %d \n",i);
         *dp++ = (float)*sp++ * mul - 0.5f;
-      
+      }
 
       printf("here\n");
     }

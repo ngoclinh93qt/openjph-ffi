907a88cb9cdf40dfac4b700ea5f38c6e4f547180
Dao Cuong <daocuong@gmail.com>
2021-08-30 14:58:11 -0500

    fix error

--- vendor/openjph/src/core/transform/ojph_colour_avx.cpp
+++ vendor/openjph/src/core/transform/ojph_colour_avx.cpp
@@ -56,6 +56,8 @@ namespace ojph {
                                        int width)
     {
       __m256 shift = _mm256_set1_ps(0.5f);
+                                    printf("err\n");
+
       __m256 m = _mm256_set1_ps(mul);
                               printf("here\n");
 

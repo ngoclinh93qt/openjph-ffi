f8ae367ee52d1628603ce9a538681e8b6a3ef2c8
Dao Cuong <daocuong@gmail.com>
2021-09-01 21:35:31 -0500

    err

--- vendor/openjph/src/core/transform/ojph_colour_sse2.cpp
+++ vendor/openjph/src/core/transform/ojph_colour_sse2.cpp
@@ -72,7 +72,6 @@ namespace ojph {
     void sse2_cnvrt_float_to_si32(const float *sp, si32 *dp, float mul,
                                  int width)
     {
-            printf("fdfdfdfdf\n");
 
       uint32_t rounding_mode = _MM_GET_ROUNDING_MODE();
       _MM_SET_ROUNDING_MODE(_MM_ROUND_NEAREST);
